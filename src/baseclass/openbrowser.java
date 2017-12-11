73a182b9abf512c9b8841090d6ebcbb0a8a6c293
ssoban <syedmohdsoban@gmail.com>
2017-12-11 21:14:56 +0530

    third commit

--- src/baseclass/openbrowser.java
+++ src/baseclass/openbrowser.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 
 public class openbrowser {
 	//this is second commit
+	//this is third commit
 	public static Logger logger = Logger.getLogger(openbrowser.class.getClass());
 
 	public WebDriver driver;
